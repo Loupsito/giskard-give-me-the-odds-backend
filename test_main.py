4ff854d049ebc59fcb7a94b217610ef273a1c967
AlexandreS <virya.suy@gmail.com>
2023-03-04 00:00:02 +0100

    tests: add integration tests for /give_me_the_odds

--- test_main.py
+++ test_main.py
@@ -5,17 +5,67 @@ from main import app
 client = TestClient(app)
 
 
-def test_concatenate_strings():
+def test_give_me_the_odds():
     response = client.post(
-        "/concatenate_strings",
-        json={"string1": "Hello", "string2": "World"},
+        "/give_me_the_odds",
+        json={
+            "millennium_falcon": {
+                "autonomy": 6,
+                "departure": "Tatooine",
+                "arrival": "Endor",
+                "routes_db": "universe.db"
+            },
+            "empire": {
+                "countdown": 8,
+                "bounty_hunters": [
+                    {
+                        "planet": "Hoth",
+                        "day": 6
+                    },
+                    {
+                        "planet": "Hoth",
+                        "day": 7
+                    },
+                    {
+                        "planet": "Hoth",
+                        "day": 8
+                    }
+                ]
+            }
+        },
     )
     assert response.status_code == 200
-    assert response.json() == "HelloWorld"
+    assert response.text == '{"value":81.0}'
 
+
+def test_give_me_the_odds_with_countdown_too_short():
     response = client.post(
-        "/concatenate_strings",
-        json={"string1": "Fast", "string2": "API"},
+        "/give_me_the_odds",
+        json={
+            "millennium_falcon": {
+                "autonomy": 6,
+                "departure": "Tatooine",
+                "arrival": "Endor",
+                "routes_db": "universe.db"
+            },
+            "empire": {
+                "countdown": 7,
+                "bounty_hunters": [
+                    {
+                        "planet": "Hoth",
+                        "day": 6
+                    },
+                    {
+                        "planet": "Hoth",
+                        "day": 7
+                    },
+                    {
+                        "planet": "Hoth",
+                        "day": 8
+                    }
+                ]
+            }
+        },
     )
     assert response.status_code == 200
-    assert response.json() == "FastAPI"
+    assert response.text == '{"value":0}'
